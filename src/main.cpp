8ca82ef73ec501f235f73f7b03fe5370866704f4
Ruben Dorado <ruben.dorados@gmail.com>
2020-11-27 22:26:19 -0500

    Add main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,39 @@
-#include <stdio.h>
-#include "foo.h"
- 
-int main(void)
-{
-    puts("This is a shared library test...");
-    foo();
-    return 0;
+#include <iostream>
+#include <map>
+
+#include <libnlp>
+
+using namespace std;
+
+/*map<string, int> config;
+
+
+class Argument{
+	string name;
+	bool isList;
+}
+
+class Command{
+	string name;
+	map<string, Argument> arguments;
+};*/
+
+void print_help();
+
+int main(int argc, char *argv[]) {
+	if (argc == 1) 
+	{
+		print_help();
+	}
+	else
+	{
+		//sentdetect();
+	}
+  
+	return 0;
+}
+
+void print_help() {
+	cout << "This is the CLI of libnlp. Current options are:" << endl;
+	cout << "  sentdetect" << endl; 
 }
